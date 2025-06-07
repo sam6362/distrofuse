b38138dafb3b5b565fba415c5860e005c00879d2
p0unter <p0unter@proton.me>
2025-06-07 16:04:46 +0300

    +global access

---
+++ build/input/CMakeFiles/input_parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/input_parser.dir/parser.c.o"
+  "CMakeFiles/input_parser.dir/parser.c.o.d"
+  "libinput_parser.a"
+  "libinput_parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/input_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
